5dde49adc2ad1f48b49262556f0a1a39b9ba9d49
lukas <lelukeonfly@gmail.com>
2023-08-23 21:22:25 +0200

    done with little bugs

--- src/App.vue
+++ src/App.vue
@@ -1,173 +1,212 @@
 <template>
-    <main class="flex flex-col gap-3">
-        <div class="flex gap-3 justify-between">
-            <div class="flex flex-col">
-                <label for="min">min: </label>
-                <input v-model="min" id="min" type="number">
-            </div>
-            <div class="flex flex-col">
-                <label for="max">max: </label>
-                <input v-model="max" id="max" type="number">
-            </div>
-            <div class="flex flex-col">
-                <label for="step">step: </label>
-                <input v-model="step" id="step" type="number">
-            </div>
-        </div>
-        <div class="flex gap-8 justify-between">
-            <div class="flex flex-col w-full">
-                <label for="slider">value: </label>
-                <input v-model="input" id="slider" type="range" :min="min" :max="max" :step="step">
-            </div>
-            <div class="flex flex-col">
-                <label for="value">value: </label>
-                <input v-model="input" id="value" type="number">
-            </div>
-        </div>
-        <div class="flex gap-3 justify-between">
-            <div class="flex flex-col">
-                <label for="f1">f1(x): </label>
-                <input v-model="f1" id="f1" type="text">
-            </div>
-            <div class="flex flex-col">
-                <label for="f2">f2(x): </label>
-                <input v-model="f2" id="f2" type="text">
-            </div>
-        </div>
-        <canvas id="fone"></canvas>
-        <canvas id="ftwo"></canvas>
-    </main>
+  <main class="flex flex-col gap-3">
+    <div class="flex gap-3 justify-between">
+      <div class="flex flex-col">
+        <label for="min">min: </label>
+        <input v-model="min" id="min" type="number">
+      </div>
+      <div class="flex flex-col">
+        <label for="max">max: </label>
+        <input v-model="max" id="max" type="number">
+      </div>
+      <!--div class="flex flex-col">
+        <label for="step">step: </label>
+        <input v-model="step" id="step" type="number">
+      </div-->
+    </div>
+    <div class="flex gap-8 justify-between">
+      <div class="flex flex-col w-full">
+        <label for="slider">value: </label>
+        <input v-model="input" id="slider" type="range" :min="min" :max="max" :step="step">
+      </div>
+      <div class="flex flex-col">
+        <label for="value">value: </label>
+        <input v-model="input" id="value" type="number">
+      </div>
+    </div>
+    <div class="flex gap-3 justify-between">
+      <div class="flex flex-col">
+        <label for="f1">f1(x): </label>
+        <input v-model="f1" id="f1" type="text">
+      </div>
+      <div class="flex flex-col">
+        <label for="f2">f2(x): </label>
+        <input v-model="f2" id="f2" type="text">
+      </div>
+    </div>
+    <canvas ref="chartCanvas"></canvas>
+    <canvas ref="chart2Canvas"></canvas>
+  </main>
 </template>
 
 <script setup>
-    import { ref, watch, onMounted, nextTick } from 'vue';
-    import Title from './components/Title.vue'
-    import * as math from 'mathjs';
-    import { Chart as ChartJS, LineElement, LinearScale, registerables } from 'chart.js';
+import { ref, watch, onMounted } from 'vue';
+import * as math from 'mathjs';
+import { Chart as ChartJS, registerables } from 'chart.js';
 
-    ChartJS.register(...registerables);
+ChartJS.register(...registerables);
 
-    const input = ref(10);
-    const min = ref(0);
-    const max = ref(15);
-    const step = ref(0.5);
-    const f1 = ref("1/2 x - 2");
-    const f2 = ref("-1/4 x + 5");
+const chartCanvas = ref(null);
+const chart2Canvas = ref(null);
 
-    let xValues = [];
-    let yValues = [];
+function createChart(canvas, data, options) {
+  const ctx = canvas.getContext('2d');
+  return new ChartJS(ctx, {
+    type: 'line',
+    data,
+    options,
+  });
+}
 
-    // show function with sample points
-    function generateData(f, min, max, step = 1) {
-        xValues = [];
-        yValues = [];
-        for (let i = min; i <= max; i += step) {
-            let y;
-            try{
-                y = math.evaluate(f, {x: i})
-            }catch(e){
-                console.error(e)
-            }
-            yValues.push(y);
-            xValues.push(i);
-        }
-        chart.data.labels = xValues;
-        chart.data.datasets[0].data = yValues;
-        chart.update();
-        //console.log(xValues)
-        //console.log(yValues)
+const input = ref(10);
+const min = ref(0);
+const max = ref(15);
+//const step = ref(0.5);
+const step = .5;
+const f1 = ref("1/2 x - 2");
+const f2 = ref("-1/4 x + 5");
+
+/*function getStep(){
+    if(step <= 0){
+        return 1
     }
+    return step.value
+}*/
 
-    // calc point
-    function generatePoint(f, x){
-        const yValue = math.evaluate(f, {x: parseFloat(x)});
-        chart.data.datasets[1].data = [{x: parseFloat(x), y: yValue}];
-        chart.update()
-        //console.log(chart.data.datasets[1].data)
+const options = {
+
+    scales: {
+      y: {
+        beginAtZero: true,
+        min: 0,
+        grid: {
+          borderColor: 'red', // Set the grid border color
+          color: 'white', // Set the grid line color
+          borderWidth: 1, // Set the grid border width
+        },
+      },
+      x: {
+        beginAtZero: true,
+        min: 0,
+        grid: {
+          borderColor: 'red', // Set the grid border color
+          color: 'white', // Set the grid line color
+          borderWidth: 1, // Set the grid border width
+        },
+      },
+    },
+    legend: { display: false },
+    title: {
+      display: true,
+      fontSize: 16,
+    },
+  }
+
+let xValues1 = [];
+let yValues1 = [];
+let xValues2 = [];
+let yValues2 = [];
+
+const data1 = {
+  labels: xValues1,
+  datasets: [
+    {
+      label: 'function',
+      fill: false,
+      pointRadius: 1,
+      borderColor: "rgba(255,0,0,0.5)",
+      data: yValues1,
+    },
+    {
+      label: 'Point',
+      data: [],
+      backgroundColor: 'red',
+      pointRadius: 5,
+      pointStyle: 'circle',
+    },
+  ],
+};
+
+const data2 = {
+  labels: xValues2,
+  datasets: [
+    {
+      label: 'function',
+      fill: false,
+      pointRadius: 1,
+      borderColor: "rgba(255,0,0,0.5)",
+      data: yValues2,
+    },
+    {
+      label: 'Point',
+      data: [],
+      backgroundColor: 'red',
+      pointRadius: 5,
+      pointStyle: 'circle',
+    },
+  ],
+};
+
+function generateData(c, f, min, max, step = 1) {
+  const xValues = c === chart ? xValues1 : xValues2;
+  const yValues = c === chart ? yValues1 : yValues2;
+
+  xValues.length = 0;
+  yValues.length = 0;
+
+  for (let i = min; i <= max; i += step) {
+    let y;
+    try {
+      y = math.evaluate(f, { x: i });
+    } catch (e) {
+      console.error(e);
     }
+    yValues.push(y);
+    xValues.push(i);
+  }
 
-    let chart = [];
+  c.data.labels = xValues;
+  c.data.datasets[0].data = yValues;
+  c.update();
+}
+function generatePoint(c, fin, x) {
+  const yValue = math.evaluate(fin, { x: parseFloat(x) });
+  //console.log(yValue);
+  c.data.datasets[1].data = [{ x: parseFloat(x), y: yValue }];
+  c.update();
+}
 
-    // load chart when site loaded
-    onMounted(async () => {
+let chart;
+let chart2;
 
-        chart = new ChartJS("fone", {
-            type: "line",
-            data: {
-                labels: xValues,
-                datasets: [{
-                    label: 'f1',
-                    fill: false,
-                    pointRadius: 1,
-                    borderColor: "rgba(255,0,0,0.5)",
-                    data: yValues,
-                    },{
-                      label: 'Point',
-                      data: [],
-                      backgroundColor: 'red', // Customize the color of the point
-                      pointRadius: 5, // Customize the size of the point
-                      pointStyle: 'circle' // Customize the shape of the point
-                }
-                ],
-            },
-            options: {
-                scales: {
-                    y: {
-                        beginAtZero: true,
-                        min: 0,
-                        grid: {
-                          borderColor: 'red', // Set the grid border color
-                          color: 'white', // Set the grid line color
-                          borderWidth: 1, // Set the grid border width
-                        },
-                    },
-                    x: {
-                        beginAtZero: true,
-                        min: 0,
-                        grid: {
-                          borderColor: 'red', // Set the grid border color
-                          color: 'white', // Set the grid line color
-                          borderWidth: 1, // Set the grid border width
-                        },
-                    },
-                },
-                legend: { display: false },
-                title: {
-                    display: true,
-                    text: "y = x * 2 + 7",
-                    fontSize: 16,
-                },
-            },
-        });
+onMounted(async () => {
+  chart = createChart(chartCanvas.value, data1, options);
+  chart2 = createChart(chart2Canvas.value, data2, options);
+  generateData(chart, f1.value, min.value, max.value, step);
+  generateData(chart2, f2.value, min.value, max.value, step);
+  generatePoint(chart, f1.value, input.value);
+  generatePoint(chart2, f2.value, input.value);
+});
 
-        // generate first view with default
-        generateData(f1.value, min.value, max.value, step.value);
-        generatePoint(f1.value, input.value)
+watch([min, max, /*step, */f1, f2], () => {
+    if(step <= 0){
+        step = 1
+    }
+  generateData(chart, f1.value, min.value, max.value, step);
+  generateData(chart2, f2.value, min.value, max.value, step);
+});
 
-    });
-
-
-    // update chart
-    watch([min, max, step, f1], () => {
-        generateData(f1.value, min.value, max.value, step.value);
-        chart.update();
-    })
-
-    // update point
-    watch(input, () => {
-        generatePoint(f1.value, input.value)
-    })
-
+watch(input, () => {
+  generatePoint(chart, f1.value, input.value);
+  generatePoint(chart2, f2.value, input.value);
+});
 </script>
 
 <style scoped>
-
 input {
     color: black;
 }
-
 * {
     color: white;
 }
-
 </style>
